e09e605ed06f7b7169b45a354c769df70f0ae71a
Lior Sandov <lior.sandov@biocatch.com>
2023-03-26 00:24:55 +0300

    build

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 
 function App() {
   const [start, setStart] = useState(false);
-  const [buttonA, setButtonA] = useState(false);
-  const [buttonB, setButtonB] = useState(false);
-  const [buttonHover, setButtonHover] = useState(false);
   const [buttonPointerEnterA, setButtonPointerEnterA] = useState(false);
   const [buttonPointerEnterB, setButtonPointerEnterB] = useState(false);
   const [buttonPointerEnterC, setButtonPointerEnterC] = useState(false);
@@ -15,101 +12,33 @@ function App() {
   function vibrate(ms) {
     navigator.vibrate(ms);
   }
-  function vibrateP() {
-    navigator.vibrate([300, 100, 300, 100, 300]);
-  }
-
-
-  const [seconds, setSeconds] = useState(0);
-
-
-  // // const myInterval = setInterval(myTimer, 1000);
-
-  // function myTimer() {
-  //   console.log('test - myTimer')
-  // }
-
-  // function myStopFunction() {
-  //   // clearInterval(myInterval);
-  //   clearInterval(interval)
-  // }
-
-  // const interval = () => setInterval(() => {
-  //   navigator.vibrate(1000);
-  //   console.log('test - interval')
-  //   if (superB) {
-
-  //   }
-  // }, 2000);
-
-  // const clean = () => {
-  //   // console.log('Interval clean')
-  //   clearInterval(interval())
-  // }
-
-  function renderELM() {
-    if (buttonA && buttonB) {
-      return <>button A | button B</>
-    }
-    if (buttonA) {
-      return <>button A</>
-    }
-    if (buttonB) {
-      return <>button B</>
-    }
-  }
-
-  // useEffect(() => {
-  //   const interval = setInterval(() => {
-  //     setSeconds(seconds => seconds + 1);
-  //   }, 1000);
-  //   return () => clearInterval(interval);
-  // }, []);
 
   useEffect(() => {
     if (start) {
-      if (buttonA && buttonB) {
-        setSuperB(false)
-        return
-      };
       if (buttonPointerEnterA && buttonPointerEnterB && buttonPointerEnterC && buttonPointerEnterD) {
         setSuperB(false)
         return
       };
       const interval = setInterval(() => {
-        setSeconds(seconds => seconds + 1);
         setSuperB(!superB)
         vibrate(1000)
-      }, 1000);
-      return () => {
-        clearInterval(interval);
-        console.log(seconds);
-        console.log(superB)
-      };
+      }, 1000)
+      return () => clearInterval(interval);
     }
-  }, [buttonA, buttonB, seconds, start, superB]);
+  }, [
+    buttonPointerEnterA,
+    buttonPointerEnterB,
+    buttonPointerEnterC,
+    buttonPointerEnterD,
+    start, superB]);
 
   return (
     <div className="App">
-      {/* <MainAppBar view={view} setView={setView} /> */}
       <header className={`App-header ${superB ? 'flash' : ''}`}>
-        {/* {renderELM()} */}
-        {/* {buttonHover ? <>onMouseOver</> : <></>} */}
-        {/* {buttonPointerEnter ? <>onPointerEnter</> : <></>} */}
-        {/* {buttonPointerEnterB ? <>onPointerEnterB</> : <></>} */}
         <div style={{ display: 'flex', margin: 'auto', flexDirection: 'column', gap: '5rem', width: '90%' }}>
-
-          {!start ? (<button style={{fontSize: '1.2rem'}} onClick={() => setStart(true)}>START</button>) : <></>}
-          {/* <div>
-            <button className='purple-button' onClick={() => setButtonA(!buttonA)}>button A</button>
-            <button className='purple-button' onClick={() => setButtonB(!buttonB)}>button B</button>
-          </div> */}
-
-          {/* <button className='purple-button' onClick={() => interval()}>startInterval</button> */}
-          {/* <button className='purple-button' onClick={() => vibrate(1000)}>1</button> */}
-          {/* <button className='purple-button' onClick={() => vibrateP()}>2</button> */}
+          {!start ? (<button style={{ fontSize: '1.2rem' }} onClick={() => setStart(true)}>START</button>) : <></>}
           {start ? (
-            <div>
+            <div style={{ display: 'flex', flexDirection: 'column', gap: '7rem' }}>
               <div style={{ display: 'flex', gap: '1rem' }}>
                 <button
                   className='purple-button'
@@ -138,11 +67,6 @@ function App() {
                 </button>) : (<div></div>)}
               </div>
             </div>) : <></>}
-          {/* <button className='purple-button' onMouseOver={() => setButtonHover(!buttonHover)}></button> */}
-          {/* <button className='purple-button' onPointerEnter={() => setButtonB(!buttonB)}>onPointerEnter - button B</button> */}
-          {/* <button className='purple-button' onClick={() => clean()}>Clear Interval</button> */}
-          {/* <button className='purple-button' onClick={() => myStopFunction()}>myStopFunction</button> */}
-          {/* {seconds} seconds have elapsed since mounting. */}
         </div>
       </header>
     </div>
